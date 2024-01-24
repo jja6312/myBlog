ca12d729cf07e936f7b64238dd3b5544ad958f67
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-01-24 23:15:28 +0900

    스타일:개발일지 글읽기 하단 글쓴이정보 구현

--- src/main/webapp/src/components/devlog/read/ReadForm.js
+++ src/main/webapp/src/components/devlog/read/ReadForm.js
@@ -7,6 +7,9 @@ import "./notionAPICustom.css";
 import DevlogTag from "./DevlogTag";
 import DevlogCategories from "./DevlogCategories";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenClip } from "@fortawesome/free-solid-svg-icons";
+
 // 개발일지 읽기 페이지 [24.01.24 15:46 정지안]
 const ReadForm = () => {
   const [response, setResponse] = useState({}); // Notion API로 받아온 데이터 저장
@@ -57,6 +60,33 @@ const ReadForm = () => {
             hideHeader={true}
             darkMode={true}
           />
+        </div>
+        {/* 작성자 정보 */}
+        <div className="flex flex-col w-full">
+          <div className="relative flex justify-start items-center">
+            <span className="text-xl">Writen</span>
+            <FontAwesomeIcon
+              className="absolute -right-9 bottom-0 text-4xl"
+              icon={faPenClip}
+            />
+          </div>
+          <hr className="mb-6"></hr>
+          <div className="flex">
+            {/* 프로필 사진 */}
+            <div className="flex justify-center items-center rounded-full overflow-hidden w-60 h-60 ">
+              <img
+                alt=""
+                className="w-full md:w-[120%] object-cover "
+                src={`${process.env.PUBLIC_URL}/image/profile/profile2.png`}
+              />
+            </div>
+            <div className="flex flex-col justify-center ml-4 space-y-4">
+              <span className="text-3xl font-semibold">정지안</span>
+              <span className="text-xl">
+                풀스택 공부를 지향하는 백엔드 개발자
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
