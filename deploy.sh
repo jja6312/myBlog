2e8e92fa540f81ed6be69e1f54f4329e7cb042be
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-02-02 16:37:12 +0900

    배포:AWS Secrets Manager로 RDS Mysql 관련정보 환경변수로 지정

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+# AWS Secrets Manager에서 환경 변수 조회
+SECRET_NAME="myBlogDatabase"
+REGION="ap-northeast-2"
+
+# AWS CLI를 사용하여 비밀 값을 조회
+SECRET=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --region $REGION --query SecretString --output text)
+
+# 조회된 비밀 값을 환경 변수로 설정
+export DATABASE_URL=$(echo $SECRET | jq -r .DATABASE_URL)
+export DATABASE_USERNAME=$(echo $SECRET | jq -r .DATABASE_USERNAME)
+export DATABASE_PASSWORD=$(echo $SECRET | jq -r .DATABASE_PASSWORD)
+
 # Spring Boot 애플리케이션 디렉터리
 SPRINGBOOT_DIR="/opt/myBlog/springboot"
 
 # 이전에 실행된 Spring Boot 애플리케이션 종료
-CURRENT_PID=$(pgrep -f '.jar')
+CURRENT_PID=$(pgrep -f 'myBlog.*.jar')
 if [ -n "$CURRENT_PID" ]; then
     echo "Stopping running Spring Boot application"
     kill -15 "$CURRENT_PID"
 fi
 
+# 잠시 대기
+sleep 5
+
 # 새 Spring Boot 애플리케이션 배포
 JAR_NAME=$(ls $SPRINGBOOT_DIR | grep '.jar' | tail -n 1)
 echo "Deploying $JAR_NAME"
-nohup java -jar $SPRINGBOOT_DIR/$JAR_NAME > $SPRINGBOOT_DIR/nohup.out 2>&1 &
+nohup java -jar $SPRINGBOOT_DIR/$JAR_NAME > $SPRINGBOOT_DIR/$(date +%F_%T)-nohup.out 2>&1 &
