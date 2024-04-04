2296da7eb39caa166aeb44b3678c687edae89e9b
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-04-04 20:33:29 +0900

    스타일:투두리스트, 2024-04-04 이전 날짜 선택시 내용 표시 불가 안내 모달

--- src/main/webapp/src/components/main/mainMiddle/todolist/TodoList.js
+++ src/main/webapp/src/components/main/mainMiddle/todolist/TodoList.js
@@ -2,8 +2,10 @@ import React from "react";
 import MemoList from "./MemoList";
 
 import ClickedDate from "./ClickedDate";
+import { useStudyTimeStore } from "../../../../store/StudyTimeStore";
 
 const TodoList = () => {
+  const { clickedDate } = useStudyTimeStore();
   return (
     <div className="w-[99.6%] flex flex-col bg-dark">
       <div className="flex items-center ">
@@ -17,7 +19,15 @@ const TodoList = () => {
         <span className="pl-2 text-2xl font-semibold">SOFTWARE IN BLOG</span>
         <ClickedDate></ClickedDate>
       </div>
-      <div className="grid grid-cols-3 gap-2 mt-2">
+      <div className="grid grid-cols-3 gap-2 mt-2 relative">
+        {/* clickedDate가 2024-04-04일 이전이라면 */}
+        {clickedDate < "2024-04-04" && (
+          <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 flex justify-center items-center border border-white">
+            <span className="text-white text-2xl">
+              해당 기능은 2024-04-04 이후 날짜부터 조회가 가능합니다.
+            </span>
+          </div>
+        )}
         <MemoList title="TODOLIST"></MemoList>
         <MemoList title="PROGRESS"></MemoList>
         <MemoList title="DONE"></MemoList>
