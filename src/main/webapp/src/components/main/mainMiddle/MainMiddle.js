3de7c5d5cd9f4999041bd098e880dcc474e3268d
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-04-07 23:18:20 +0900

    수정:프로젝트, 로딩 스타일 수정

--- src/main/webapp/src/components/main/mainMiddle/MainMiddle.js
+++ src/main/webapp/src/components/main/mainMiddle/MainMiddle.js
@@ -14,7 +14,7 @@ const MainMiddle = () => {
   const getYearlyStudyTime = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:8080/studyTime/getYearlyStudyTime"
+        "http://43.203.18.91:8080/studyTime/getYearlyStudyTime"
       );
       setYearlyStudyTime(response.data);
       console.log("yearlyStudyTime", response.data);
